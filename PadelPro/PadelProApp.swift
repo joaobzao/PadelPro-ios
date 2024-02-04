f1f65e8f48927a146bebb5d3a9fb2d1d776fdd89
joaobzao <jmmbzao@gmail.com>
2024-02-04 12:18:33 +0000

    create ios project

---
+++ PadelPro/PadelProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelProApp.swift
+//  PadelPro
+//
+//  Created by Joao Zao on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PadelProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
