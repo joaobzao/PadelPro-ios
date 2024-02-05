e3c7c909b9ef07806e71311524902611a0370d9e
joaobzao <jmmbzao@gmail.com>
2024-02-05 02:02:10 +0000

    create filter

--- PadelPro/Event.swift
+++ PadelPro/Event.swift
@@ -6,26 +6,52 @@
 //
 
 import ComposableArchitecture
-import Foundation
+import SwiftUI
 
-//@Reducer
-//struct Event {
-//    @ObservableState
-//    struct State: Equatable, Identifiable {
-//        let id: UUID
-//        var name: String = ""
-//    }
-//    
-//    enum Action {
-//        case favorite
-//    }
-//    
-//    var body: some Reducer<State, Action> { 
-//        Reduce { state, action in
-//            switch action {
-//            case .favorite:
-//                return .none
-//            }
-//        }
-//    }
-//}
+@Reducer
+struct Event {
+    @ObservableState
+    struct State: Equatable, Identifiable {
+        let id: UUID
+        var name: String
+        var month: String
+        var days: String
+        var type: String?
+        var location: String?
+        var isFavourite: Bool = false
+    }
+    
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some Reducer<State, Action> { 
+        BindingReducer()
+    }
+}
+
+struct EventView: View {
+    @Bindable var store: StoreOf<Event>
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(store.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            HStack {
+                Image(systemName: "calendar")
+                Text("\(store.month.lowercased().capitalized) - \(store.days)")
+                    .foregroundStyle(.gray)
+                    .fontWeight(.semibold)
+            }
+            
+            if let location = store.location {
+                HStack {
+                    Image(systemName: "location.fill")
+                    Text(location)
+                }
+            }
+        }
+    }
+}
--- PadelPro/Events.swift
+++ PadelPro/Events.swift
@@ -8,30 +8,63 @@
 import ComposableArchitecture
 import SwiftUI
 
+enum Filter: LocalizedStringKey, CaseIterable, Hashable {
+    case all = "Todos"
+    case trainning = "Formação"
+    case competition = "Competição"
+    case league = "Ligas"
+}
+
 @Reducer
 struct Events {
     @ObservableState
-    struct State {
-        var events: [EventsModel.EventModel] = []
+    struct State: Equatable {
+        var events: IdentifiedArrayOf<Event.State> = []
+        var filter: Filter = .all
+        
+        var filteredEvents: IdentifiedArrayOf<Event.State> {
+            switch filter {
+            case .all: return events
+            case .trainning: return events.filter { $0.type == "FOR" }
+            case .competition: return events.filter { $0.type == "CIR" }
+            case .league: return events.filter { $0.type == "EQU" }
+            }
+        }
     }
     
-    enum Action {
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
         case eventsResponse(Result<EventsModel, Error>)
+        case events(IdentifiedActionOf<Event>)
         case retrieveEvents
     }
     
     @Dependency(\.eventsClient) var eventsClient
+    @Dependency(\.uuid) var uuid
     private enum CancelID { case events }
     
     var body: some Reducer<State, Action> {
+        BindingReducer()
         Reduce { state, action in
             switch action {
             case .eventsResponse(.failure):
                 state.events = []
                 return .none
             case let .eventsResponse(.success(response)):
-                state.events = response.events
+                let events = response.events.map { event in
+                    Event.State(
+                        id: UUID(),
+                        name: event.name,
+                        month: event.month,
+                        days: event.days,
+                        type: event.type,
+                        location: event.location
+                    )
+                }
+                
+                state.events.insert(contentsOf: events, at: 0)
                 return .none
+                
             case .retrieveEvents:
                 return .run { send in
                     await send(
@@ -41,7 +74,16 @@ struct Events {
                     )
                 }
                 .cancellable(id: CancelID.events, cancelInFlight: true)
+            
+            case .binding:
+                return .none
+                
+            case .events(_):
+                return .none
             }
+        }
+        .forEach(\.events, action: \.events) {
+            Event()
         }
     }
 }
@@ -51,26 +93,18 @@ struct EventsView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(store.events, id: \.self) { event in
-                    VStack(alignment: .leading) {
-                        Text(event.name)
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        
-                        HStack {
-                            Image(systemName: "calendar")
-                            Text("\(event.month.lowercased().capitalized) - \(event.days)")
-                                .foregroundStyle(.gray)
-                                .fontWeight(.semibold)
-                        }
-                        
-                        if let location = event.location {
-                            HStack {
-                                Image(systemName: "location.fill")
-                                Text(location)
-                            }
-                        }
+            VStack(alignment: .leading) {
+                Picker("Filter", selection: $store.filter) {
+                  ForEach(Filter.allCases, id: \.self) { filter in
+                    Text(filter.rawValue).tag(filter)
+                  }
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
+                List {
+                    ForEach(store.scope(state: \.filteredEvents, action: \.events)) { store in
+                        EventView(store: store)
                     }
                 }
             }
