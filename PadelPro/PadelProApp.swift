9464d5ddd85150fa412c6255e451649191b28497
joaobzao <jmmbzao@gmail.com>
2024-02-06 02:29:53 +0000

    add firebase

--- PadelPro/PadelProApp.swift
+++ PadelPro/PadelProApp.swift
@@ -7,9 +7,21 @@
 
 import ComposableArchitecture
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct PadelProApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
