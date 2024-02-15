22f455acdd5f1e4bad83bf1ab700cbca5d1d5550
joaobzao <jmmbzao@gmail.com>
2024-02-15 01:25:12 +0000

    network monitor on going

--- PadelPro/Network/NetworkMonitor.swift
+++ PadelPro/Network/NetworkMonitor.swift
@@ -6,22 +6,52 @@
 //
 
 import Foundation
+import ComposableArchitecture
 import Network
 
-final class NetworkMonitor: ObservableObject {
+@Reducer
+struct NetworkMonitor {
+    @ObservableState
+    struct State: Equatable {
+        var isDisconnected: Bool = false
+    }
+    
+    enum Action: BindableAction ,Sendable {
+        case binding(BindingAction<State>)
+        case connectivityChanged(Bool)
+        case startLinteningConnectivityChanges
+    }
+    
     private let networkMonitor = NWPathMonitor()
     private let workerQueue = DispatchQueue(label: "Monitor")
     var isConnected = false
-
-    init() {
-        networkMonitor.pathUpdateHandler = { path in
-            self.isConnected = path.status == .satisfied
-            Task {
-                await MainActor.run {
-                    self.objectWillChange.send()
+    private enum CancelID {
+        case retrieveEvents
+        case searchQuerySubmit
+    }
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+        Reduce { state, action in
+            
+            switch action {
+            case let .connectivityChanged(isDisconnected):
+                state.isDisconnected = isDisconnected
+                return .none
+                
+            case .startLinteningConnectivityChanges:
+                return .run { send in
+                    for await data in networkMonitor {
+                        await send(
+                            .connectivityChanged(data.status != .satisfied)
+                        )
+                    }
+                    networkMonitor.start(queue: workerQueue)
                 }
+                .cancellable(id: CancelID.retrieveEvents, cancelInFlight: true)
+            case .binding(_):
+                return .none
             }
         }
-        networkMonitor.start(queue: workerQueue)
     }
 }
--- PadelPro/PadelProApp.swift
+++ PadelPro/PadelProApp.swift
@@ -21,96 +21,124 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct PadelProApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @State private var showNetworkAlert = false
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(initialState: NetworkMonitor.State()) {
+                    NetworkMonitor()
+                        ._printChanges()
+                }
+            )
         }
     }
 }
+import FirebaseAnalytics
 
 struct ContentView: View {
+    @Bindable var store: StoreOf<NetworkMonitor>
     @AppStorage("showOnBoarding") private var showOnBoarding: Bool = true
     var data = OnboardingDataModel.data
     
     var body: some View {
-        if showOnBoarding {
-            OnBoardingView(data: data, doneFunction: {
-                showOnBoarding = false
-            })
-        } else {
-            TabView {
-                EventsView(
-                    store: Store(initialState: Events.State()) {
-                      Events()
-                        ._printChanges()
-                    }
-                )
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text(FilterEventType.all.rawValue)
-                }
-                
-                EventsView(
-                    store: Store(
-                        initialState: Events.State(
-                            filterEventType: .competition
-                        )
-                    ) {
-                        Events()
+        Group {
+            if showOnBoarding {
+                OnBoardingView(data: data, doneFunction: {
+                    showOnBoarding = false
+                })
+//                .onAppear {
+//                    Analytics.logEvent(
+//                        AnalyticsEventScreenView,
+//                        parameters: [
+//                            AnalyticsParameterScreenName: "OnBoardingView",
+//                        ]
+//                    )
+//                }
+//                Analytics.logEvent(AnalyticsEventScreenView,
+//                                           parameters: [AnalyticsParameterScreenName: "\(ContentView.self)",
+//                                                        AnalyticsParameterScreenClass: "\(ContentView.self)"])
+            } else {
+                TabView {
+                    EventsView(
+                        store: Store(initialState: Events.State()) {
+                          Events()
                             ._printChanges()
+                        }
+                    )
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text(FilterEventType.all.rawValue)
                     }
-                )
-                .tabItem {
-                    Image(systemName: "figure.tennis")
-                    Text(FilterEventType.competition.rawValue)
-                }
-                
-                EventsView(
-                    store: Store(
-                        initialState: Events.State(
-                            filterEventType: .league
-                        )
-                    ) {
-                        Events()
-                            ._printChanges()
+                    
+                    EventsView(
+                        store: Store(
+                            initialState: Events.State(
+                                filterEventType: .competition
+                            )
+                        ) {
+                            Events()
+                                ._printChanges()
+                        }
+                    )
+                    .tabItem {
+                        Image(systemName: "figure.tennis")
+                        Text(FilterEventType.competition.rawValue)
                     }
-                )
-                .tabItem {
-                    Image(systemName: "figure.run.square.stack.fill")
-                    Text(FilterEventType.league.rawValue)
-                }
-                
-                EventsView(
-                    store: Store(
-                        initialState: Events.State(
-                            filterEventType: .trainning
-                        )
-                    ) {
-                        Events()
-                            ._printChanges()
+                    
+                    EventsView(
+                        store: Store(
+                            initialState: Events.State(
+                                filterEventType: .league
+                            )
+                        ) {
+                            Events()
+                                ._printChanges()
+                        }
+                    )
+                    .tabItem {
+                        Image(systemName: "figure.run.square.stack.fill")
+                        Text(FilterEventType.league.rawValue)
                     }
-                )
-                .tabItem {
-                    Image(systemName: "graduationcap.fill")
-                    Text(FilterEventType.trainning.rawValue)
-                }
-                
-                FavouritesView(
-                    store: Store(
-                        initialState: Favourites.State(
-                            filterEventType: .favourites
-                        )
-                    ) {
-                        Favourites()
-                            ._printChanges()
+                    
+                    EventsView(
+                        store: Store(
+                            initialState: Events.State(
+                                filterEventType: .trainning
+                            )
+                        ) {
+                            Events()
+                                ._printChanges()
+                        }
+                    )
+                    .tabItem {
+                        Image(systemName: "graduationcap.fill")
+                        Text(FilterEventType.trainning.rawValue)
                     }
-                )
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text(FilterEventType.favourites.rawValue)
+                    
+                    FavouritesView(
+                        store: Store(
+                            initialState: Favourites.State(
+                                filterEventType: .favourites
+                            )
+                        ) {
+                            Favourites()
+                                ._printChanges()
+                        }
+                    )
+                    .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text(FilterEventType.favourites.rawValue)
+                    }
                 }
             }
+        }
+        .onAppear { store.send(.startLinteningConnectivityChanges) }
+        .alert(
+            "Parece que estás sem internet!",
+            isPresented: $store.state.isDisconnected
+        ) {
+            
         }
     }
 }
